0347912dbc699acccce6efabb2264cafa89c7cd4
Sahil Soni <107581830+ssonig1@users.noreply.github.com>
2022-06-25 16:44:29 +0530

    Site files

---
+++ index.js
@@ -0,0 +1,1 @@
+var color1 = document.querySelector(".color1");var css= document.querySelector("h3");var color2 = document.querySelector(".color2");var body = document.getElementById("bg");function setgradient(){body.style.backgroundImage = ("linear-gradient(to right, "+color1.value+","+color2.value+")");css.textContent= "background-image = "+body.style.backgroundImage +";";}
